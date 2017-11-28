d9b9e15c49a9298580a5dbd4b12d1672ce6d61d9
LakshaJael <nihongotraveler@icloud.com>
2017-11-28 12:38:32 +0800

    Added a couple functions, working on receiving percentages of mostly used words compared to the whole text.

--- word_probability.rb
+++ word_probability.rb
@@ -5,7 +5,6 @@ def word_probability(file_name)
   #   JSON.parse(f.read)
   # end
   file_to_check = parse_file(file_name)
-
   make_word_hash(split_words(file_to_check))
 end
 
@@ -18,15 +17,17 @@ def split_words(file_to_check)
   return file_to_check.split(" ")
 end
 
-def read_story(finished_story)
-  File.open(finished_story, "r") {|file| file.read}
-end
-
 def make_word_hash(words)
-  word_list = Hash.new { |hash, key| hash[key] = [] }
-  words.each {|word| word_list[word] if word_list.has_key?(word) == false}
-  word_matching(word_list, words) #call another method that will check each word and return
-  #each word and put it into the key
+  word_list = Hash.new { |hash, key| hash[key] = [1] }
+  words.each do |word|
+    if word_list.has_key?(word) == false
+      word_list[word]
+    else
+      #adds 1 for each occurance of the word when the key already exists in the hash
+      word_list[word][0] += 1
+    end
+  end
+  word_matching(word_list, words)
 end
 
 def word_matching(word_list, words)
@@ -34,8 +35,19 @@ def word_matching(word_list, words)
   words.each_with_index do |word, i|
     word_list[word].push(words[i+1]) if words[i+1] != nil
   end
-  return word_list
+  words_used_often(word_list)
 end
 
+def words_used_often(word_list)
+  percentage = []
+  often_used_words = Hash.new { |hash, key| hash[key] = ""}
+  word_list.each do |key, value|
+    if value[0] > 1
+      often_used_words[key] = value[0]
+    end
+  end
+  return often_used_words
+  #often_used_words.each {|key, value| value = (value[0] / often_used_words.length)}
+end
 
-puts word_probability("written_test.txt")
+word_probability("written_test.txt")
